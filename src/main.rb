bb2e240abb49c675d7c5ab5044700b41fee57f3b
t-bowersox <11789189+t-bowersox@users.noreply.github.com>
2024-03-09 17:20:34 -0500

    Format code with Standard Ruby

--- src/main.rb
+++ src/main.rb
@@ -2,8 +2,8 @@
 
 # frozen_string_literal: true
 
-require 'time'
-require_relative 'lib/time_diff'
+require "time"
+require_relative "lib/time_diff"
 
 def exit_with_error(error_msg)
   puts error_msg
@@ -12,10 +12,10 @@ end
 
 # @param [Array<String>] times An array containing two times to compare.
 def main(times)
-  start_time, end_time = times.map { |time| time.downcase == 'now' ? Time.new : Time.parse(time) }
+  start_time, end_time = times.map { |time| (time.downcase == "now") ? Time.new : Time.parse(time) }
   puts TimeDiff.new(start_time, end_time)
 rescue ArgumentError
-  exit_with_error "Error: #{times.join(' and ')} are not valid times."
+  exit_with_error "Error: #{times.join(" and ")} are not valid times."
 rescue RuntimeError => e
   exit_with_error e.message
 end
